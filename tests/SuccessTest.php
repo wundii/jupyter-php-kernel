e886c1f18df2665f4b16ad710a9db6f9b0ba8e33
awu <andreas.wunderwald@westpress.de>
2025-08-15 06:14:57 +0200

    chore: Add initial PHPUnit test and configuration file

---
+++ tests/SuccessTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+
+use PHPUnit\Framework\TestCase;
+
+final class SuccessTest extends TestCase
+{
+    public function testSucceed(): void
+    {
+        $this->assertTrue(true);
+    }
+}
