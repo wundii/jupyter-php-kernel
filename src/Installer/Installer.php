b05d732d9ca2a0b311751d39f6fa55abc3457134
awu <andreas.wunderwald@westpress.de>
2025-08-15 07:52:24 +0200

    feat: Update kernel JSON metadata to include logo file paths

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -34,12 +34,15 @@ class Installer
 
     protected static function getKernelJSON(): array
     {
+        $kernel_path = self::getInstallPath();
         return [
             'argv' => ['jupyter-php-kernel', '-r',  '-c', '{connection_file}'],
             'display_name' => 'PHP ' . PHP_VERSION,
             'language' => 'php',
             'metadata' => [
                 'debugger' => true,
+                'file1' => __FILE__ . '/logo-32x32.png',
+                'file2' => $kernel_path . '/logo-32x32.png',
             ],
         ];
     }
